c5a5ef9fe4467d28ac21eb34cb9f25c0229560c4
Rohan Singh <rohansingh732003@gmail.com>
2026-02-06 14:47:23 +0530

    fix the navbar.js added / instead of # inside href

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = ({setCategory}) => {
   return (
    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"> <span className="badge bg-light text-dark fs-4">NewsMag</span> </a>
+    <a className="navbar-brand" href="/"> <span className="badge bg-light text-dark fs-4">NewsMag</span> </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -25,7 +25,7 @@ const Navbar = ({setCategory}) => {
           <a className="nav-link" onClick={() => setCategory("sports")}>Sports</a>
         </li>
           <li className="nav-item">
-          <a className="nav-link" href="#">Entertainment</a>
+          <a className="nav-link" href="/">Entertainment</a>
         </li>
        
       </ul>
